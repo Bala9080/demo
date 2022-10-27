ea7a3d30c1a008e9e504eccf7acb5736ac1a896c
Balamurugan Rajan <balamurugan.r@SOLVER.SVMDOMAIN>
2022-10-27 18:07:44 +0530

    First commit

---
+++ gitdemo.java
@@ -0,0 +1,26 @@
+
+ public class gitdemo {
+ 
+   
+    public static void main(String[] args)
+    {
+        
+        int num =0;
+        for(int i=num;i<=500;i++)
+ {
+       
+        if (i % 2 == 0) {
+ 
+           
+            System.out.println("Even :"+ i);
+        }
+ 
+        else {
+ 
+          
+            System.out.println("Odd :"+ i);
+        }
+    }
+}
+}
+ 
